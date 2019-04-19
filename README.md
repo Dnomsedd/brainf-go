09fd42bf52dbc67fc4f20dc112fe8f374cf9627c
Viktor Rozenko <sharp.vik@gmail.com>
2019-04-19 01:51:17 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# BrainF@ck Interpreter
+
+And all of it is written in Golang so that you can install and use it.
+
+## What is BrainF@ck
+
+BrainF@ck is an esoteric programming language created by Urban Muller in 1993. Its interpreter is extremely easy to implement, however its syntax is as minimalistic as it gets... 
+
+The general idea is that you have:
+
+1. a string of code (that was hopefully written by you);
+2. an instruction pointer that is initially set to 0 (points at the first instruction);
+3. 30'000 memory locations that store integers and are also initially set to 0s;
+4. a data pointer that points at some memory location (also set to 0 initially and points at the first memory cell).
+
+Now, the only 8 characters that interpreter cares about are:
+
+```
++ - > < . , [ ]
+```
+
+| Operator | Effect |
+|:--------:|:-------|
+| + / -    | Increment / decrement the value stored in the memory cell pointed to by the data pointer |
+| > / <    | Shift data pointer right / left |
+| ,        | Read one char from standard input and store it as an integer in the current memory cell |
+| .        | Print ASCII character that corresponds to the integer value stored in the current memory cell |
+| [ ]      | Loop while value of current memory cell is non-zero when ] is hit |
+
+You can find a lot of resources about this language in the internet. You now also have your own compact and fast BrainF@ck interpreter ;)
+
+## How to Use
+
+Run these in your terminal and it will make you just a bit happier.
+
+```
+~$ go get github.com/sharpvik/brainf-go
+~$ go install path/to/brainf-go
+~$ path/to/brainf-go(.exe) path/to/file.bf
+```
+
+## Contact
+
+For any personal or business enquiries:
+
++ Email: *sharp.vik@gmail.com*
++ [Twitter](https://twitter.com/sharp_vik)
++ [VK](https://vk.com/perigrinus)
++ [Instagram](https://www.instagram.com/viktooooor)
---
+++ main.go
@@ -0,0 +1,164 @@
+/* By Viktor A. Rozenko Voitenko (2019)
+ *
+ * BRAINFUCK SYNTAX
+ *
+ *  +  =>  memory[dp]++
+ *  -  =>  memory[dp]--
+ *  >  =>  dp++
+ *  <  =>  dp--
+ *  .  =>  fmt.Print(memory[dp])
+ *  ,  =>  reader.ReadByte()
+ *  [  =>  jump to ] if memory[dp] == 0
+ *  ]  =>  jump back to [ if memory[dp] != 0
+ *
+ */
+
+
+
+package main
+
+
+import (
+    "fmt"
+    "io/ioutil"
+    "bufio"
+    "os"
+)
+
+
+
+type BVM struct {
+    code    []byte
+    ip      int
+
+    memory  [30000]int
+    dp      int
+
+    reader  *bufio.Reader
+
+    running bool
+    err     bool
+}
+
+
+func New(src string) *BVM {
+    // read code
+    data, err := ioutil.ReadFile(src)
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    // return instance
+    return &BVM {
+        code    : data,
+        reader  : bufio.NewReader(os.Stdin),
+        running : true,
+    }
+}
+
+
+func (vm *BVM) Exec() {
+    i := vm.code[vm.ip]
+    switch i {
+        case '+':
+            vm.memory[vm.dp]++
+            vm.ip++
+
+        case '-':
+            vm.memory[vm.dp]--
+            vm.ip++
+
+        case '>':
+            vm.dp++
+            if vm.dp == 30000 {
+                vm.err = true
+                fmt.Printf("\n#%d: Data Pointer out of bounds.\n", vm.ip)
+            }
+            vm.ip++
+
+        case '<':
+            vm.dp--
+            if vm.dp == -1 {
+                vm.err = true
+                fmt.Printf("\n#%d: Data Pointer out of bounds.\n", vm.ip)
+            }
+            vm.ip++
+
+        case '.':
+            c := vm.memory[vm.dp]
+            if 0 <= c && c <= 128 {
+                fmt.Printf( string(c) )
+            } else {
+                vm.err = true
+                fmt.Printf(`\n#%d: Invalid int value for ASCII char %d
+                                   at memory location %d.\n`,
+                           vm.ip, c, vm.dp)
+            }
+            vm.ip++
+
+        case ',':
+            fmt.Print(">> ")
+            b, err := vm.reader.ReadByte()
+            if err != nil {
+                vm.err = true
+                fmt.Print("\n")
+                fmt.Println(err)
+                fmt.Print("\n")
+            } else {
+                vm.memory[vm.dp] = int(b)
+            }
+            vm.ip++
+
+        case '[':
+            if vm.memory[vm.dp] == 0 {
+                for vm.code[vm.ip] != '!' {
+                    vm.ip++
+                    if vm.code[vm.ip] == ']' {
+                        vm.ip++
+                        return
+                    }
+                }
+                vm.err = true
+                fmt.Printf("\nSyntax Error: Did not find matching ']'.\n")
+            } else {
+                vm.ip++
+            }
+
+        case ']':
+            if vm.memory[vm.dp] != 0 {
+                for vm.ip >= 0 {
+                    vm.ip--
+                    if vm.code[vm.ip] == '[' {
+                        vm.ip++
+                        return
+                    }
+                }
+                vm.err = true
+                fmt.Printf("\nSyntax Error: Did not find matching '['.\n")
+            } else {
+                vm.ip++
+            }
+
+        case '!':
+            vm.running = false
+
+        default:
+            vm.ip++ // don't pay attention to unknown command
+    }
+}
+
+
+
+func main() {
+    src := os.Args[1] // grab the source code filename
+    vm := New(src)
+
+    for vm.running && !vm.err {
+        vm.Exec()
+    }
+
+    if vm.err {
+        fmt.Println("\nBrainfuck exited with error.\n")
+    }
+}
+
